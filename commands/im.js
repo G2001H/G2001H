35284867fb04a899b31292bf9e909fbbc8cbe3b1
G2001H <57664563+G2001H@users.noreply.github.com>
2019-12-04 12:46:31 +0100

    Update im.js

--- commands/im.js
+++ commands/im.js
@@ -1,8 +1,4 @@
 module.exports.run = async (bot, message, args) => {
-    if(args[0].toLowerCase() === "verified?"){
-        if(!message.member.roles.find(r => r.name === "Verified")) return message.reply('No your not!')
-        message.reply('Yes you are!')
-    };
     if(args[0].toLowerCase() === "owner?"){
         if(!message.member.ID === message.guild.ownerID) return message.reply('No your not!')
         message.reply('Yes you are!')
