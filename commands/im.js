398bef938fe2405ecfc5735f9f8d584df2dd9102
G2001H <57664563+G2001H@users.noreply.github.com>
2019-12-04 14:14:38 +0100

    Delete im.js

--- commands/im.js
+++
@@ -1,6 +0,0 @@
-module.exports.run = async (bot, message, args) => {
-    if(args[0].toLowerCase() === "owner?"){
-        if(!message.author.id === message.guild.ownerID) return message.reply('No your not!')
-        message.reply('Yes you are!')
-    };
-}
