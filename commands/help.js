0905374af40b5df1f3c84190adfaf11f7ef5b2ba
G2001H <57664563+G2001H@users.noreply.github.com>
2019-12-04 13:23:06 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -3,16 +3,9 @@ const Discord = require('discord.js')
 module.exports.run = async (bot, message, args) => {
     let embed1 = new Discord.RichEmbed()
     .setColor("RANDOM")
-    .setTitle('**Commands For Members:**')
-    .addField('**__Prefix:__**', "`;`")
-    .addField('**__Free Commands:__**', '`;Help`, `;Ascii <Text>`, `;Triggered <@Member>`, `;Image <Text>`, `;Avatar <@Member>`, `;Minecraft <ServerID>`, `;Kill <@Member>`, `;ServerInfo`, `;Info <@Member>`, `;Ping`, `;Im Owner?`, `;8ball <Text>`, `;Truth`, `;Dare`')
-    .setFooter('Bot by: G2001H#7660')
-    let embed2 = new Discord.RichEmbed()
-    .setColor("RANDOM")
-    .setTitle('**Commands For Owners:**')
-    .addField('**__Prefix:__**', "`;`")
-    .addField('**__Owner Commands:__**', "`;Purge <1-100>`, `;Kick <@Member> <Reason>`, `;Vote <@Member>`, `;Mute <@Member> <time> <Reason>`, `;Ban <@Member> <Reason>`, `;Poll <Text>`, `;ServerSetName <Text>`")
+    .setTitle('🕹️ **__All Commands:__** 🕹️')
+    .addField('☑️ **__Free Commands:__**',' `;Help` | `;asciiface` | `;triggered <@Member>` | `;ascii <Text>` | `Image <Text>` | `;Avatar <@Member>` | `;Minecraft <ServerID>` | `;Kill <@Member>` | `;ServerInfo` | `;Info <@Member>` | `;Ping` | `;Im Owner?` | `;8ball <Text>` | `;Truth` | `;Dare`')
+    .addField('👑 **__Administrator Commands:__**', "`;Purge <1-100>` | `;Kick <@Member> <Reason>` | `;Vote <@Member>` | `;Mute <@Member> <time> <Reason>` | `;Ban <@Member> <Reason>` | `;Poll <Text>` | `;ServerSetName <Text>`")
     .setFooter('Bot by: G2001H#7660')
     message.channel.send(embed1);
-    message.channel.send(embed2);
 }
