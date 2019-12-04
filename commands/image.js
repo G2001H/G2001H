f0d186dbc18a30e3ff457862386fe1e07c9fda6c
G2001H <57664563+G2001H@users.noreply.github.com>
2019-12-04 19:20:04 +0100

    Update image.js

--- commands/image.js
+++ commands/image.js
@@ -1,14 +1,31 @@
-const Discord = require("discord.js");
-const randomPuppy = require("random-puppy");
+var cheerio = require("cheerio");
+var request = require("request"); 
+var Discord = require("discord.js");
 
-module.exports.run = async (client, message, args) => {
-    if(!args[0]) return message.reply('`;Image <Text>`')
-    const img = await randomPuppy(args[0]);
-    const embed = new Discord.RichEmbed()
+module.exports.run = async (bot, message, args) => {
+    var search = args.join(" ");
+    var options = {
+        url: "http://results.dogpile.com/serp?qc=images&q=" + search,
+        method: "GET",
+        headers: {
+            "Accept": "text/html",
+            "User-Agent": "Chrome"
+        }
+    };
+    request(options, function(error, response, responseBody) {
+        if (error) {
+            return;
+        }
+        $ = cheerio.load(responseBody);
+        var links = $(".image a.link");
+        var urls = new Array(links.length).fill(0).map((v, i) => links.eq(i).attr("href"));
+        if (!urls.length) {
+            return;
+        }
+        const embeds = new Discord.RichEmbed()
         .setColor("RANDOM")
-        .setImage(img)
-        .setTitle("Image " + args[0])
-        .setURL(`https://reddit.com/r/${args[0]}`)
-        .setFooter('Bot by: G2001H#7660');
-    message.channel.send(embed);
+        .setTitle(`Image: ${search}`)
+        .setImage(urls[0])
+        message.channel.send(embeds);
+    });
 }
