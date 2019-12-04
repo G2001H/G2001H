3eed22d9e740d96b1722e88bd25d1cc2ff6d5ba3
G2001H <57664563+G2001H@users.noreply.github.com>
2019-12-04 12:43:09 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -3,8 +3,7 @@ const Discord = require('discord.js')
 module.exports.run = async (bot, message, args) => {
     let embed = new Discord.RichEmbed()
     .setColor("RANDOM")
-    .setTitle('**🏓Pong**')
-    .addField('**__Bot Ping__**', `My ping is **${Math.round(bot.ping)} ms**!`)
+    .setTitle(`**Pong** 🏓 **${Math.round(bot.ping)} ms**!`)
     .setFooter('Bot by: G2001H#7660')
     message.channel.send(embed)
 }
