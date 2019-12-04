f2dda3d307441be9417b95dfec7b321a8d0729ab
G2001H <57664563+G2001H@users.noreply.github.com>
2019-12-04 14:13:21 +0100

    Update im.js

--- commands/im.js
+++ commands/im.js
@@ -1,6 +1,6 @@
 module.exports.run = async (bot, message, args) => {
     if(args[0].toLowerCase() === "owner?"){
-        if(!message.author.user.id === message.guild.ownerID) return message.reply('No your not!')
+        if(!message.author.id === message.guild.ownerID) return message.reply('No your not!')
         message.reply('Yes you are!')
     };
 }
