2e94cbadc04e036b95033706a35e68abe6bf42ae
G2001H <57664563+G2001H@users.noreply.github.com>
2019-12-08 14:41:48 +0100

    Delete kill.js

--- commands/kill.js
+++
@@ -1,9 +0,0 @@
-module.exports.run = async (bot, message, args) => {
-    const msgArgs3 = message.mentions.users.first() 
-    if(!msgArgs3) return message.reply('`;Kill <@Member>`');{
-    const memberss = message.guild.member(msgArgs3);
-    if (!memberss) return message.reply("Invalid Member!");{
-        message.channel.send(msgArgs3 + " **Was Killed by** " + `${message.author}` + "! 🔪")
-        message.delete(1000).catch(console.error);
-    }};
-}
