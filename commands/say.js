0b4dd78af434770169b9687adb247e1c3efc683a
G2001H <57664563+G2001H@users.noreply.github.com>
2019-12-08 14:42:21 +0100

    Delete say.js

--- commands/say.js
+++
@@ -1,5 +0,0 @@
-module.exports.run = async (bot, message, args) => {
-    const says = args.join(" ");
-    if(!says) return message.reply("`;Say <Channel> <Text>`");
-    message.channel.send(says);
-}
