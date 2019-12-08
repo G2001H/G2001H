8d14b2c3cdf51d481aabb99d914b2525fd2dbfc0
G2001H <57664563+G2001H@users.noreply.github.com>
2019-12-08 14:41:05 +0100

    Delete channelsay.js

--- commands/channelsay.js
+++
@@ -1,8 +0,0 @@
-module.exports.run = async (bot, message, args) => {
-    if (!message.member.hasPermission('ADMINISTRATOR')) return message.reply('You dont have permission to use this command!');
-    const channels = message.guild.channels.find(ch => ch.name === args[0]);
-    const says = args.slice(1).join(" ");
-    if(!channels) return message.reply("`;ChannelSay <Channel> <Text>`");
-    if(!says) return message.reply("`;ChannelSay <Channel> <Text>`");
-    channels.send(says);
-}
