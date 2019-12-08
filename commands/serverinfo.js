27b8acb929507add5b4ed7037f97dfa5f4f85789
G2001H <57664563+G2001H@users.noreply.github.com>
2019-12-08 14:54:54 +0100

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -6,11 +6,11 @@ module.exports.run = async (bot, message, args) => {
     .setTitle('📜 **Server Informations:** 📜')
     .addField('🌟 **__Server Name:__**', message.guild.name,true)
     .addField('🌟 **__Server ID:__**', message.guild.id, true)
+    .addField('👑 **__Owner:__**', message.guild.owner,true)
     .addField('📅 **__Server Created:__**', message.guild.createdAt)
     .addField('🙋‍♂️ **__Members Count:__**', message.guild.memberCount, true)
     .addField('🔘 **__Roles Count:__**', message.guild.roles.size - 1, true)
     .addField('👌 **__Channels Count:__**', message.guild.channels.size, true)
-    .addField('👑 **__Owner:__**', message.guild.owner,true)
     .setThumbnail(message.guild.iconURL)
     .setFooter('Bot by: G2001H#7660')
      message.channel.send(embed);
