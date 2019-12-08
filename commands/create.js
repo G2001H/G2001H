f46b8a2fda003866e3cdd6007e0e0ad4bd10d491
G2001H <57664563+G2001H@users.noreply.github.com>
2019-12-08 15:36:37 +0100

    Update create.js

--- commands/create.js
+++ commands/create.js
@@ -1,6 +1,6 @@
 module.exports.run = async (bot, message, args) => {
 if(!message.member.hasPermission('ADMINISTRATOR')) return message.reply('You dont have permission to use this command!');
-if(args[0].toLowerCase() === "logs"){
+if(args[0].toLowerCase() === "channel logs"){
     if(!message.guild.channels.find(channel => channel.name === "logs")){
           message.guild.createChannel("logs",`TextChannel`)
           message.reply("Succsessfuly created a channel (logs)!")
@@ -8,7 +8,7 @@ if(args[0].toLowerCase() === "logs"){
         message.reply("You already have a channel (logs)!")
     }
 }
-if(args[0].toLowerCase() === "rolemuted"){
+if(args[0].toLowerCase() === "role muted"){
     if(!message.guild.roles.find(role => role.name === "Muted")){  
         message.guild.createRole({
             name: "Muted",
@@ -20,7 +20,7 @@ if(args[0].toLowerCase() === "rolemuted"){
         message.reply("You already have a role (Muted)!")
     }
 }
-if(args[0].toLowerCase() === "roleverified"){
+if(args[0].toLowerCase() === "roleve rified"){
     if(!message.guild.roles.find(role => role.name === "Verified")){  
         message.guild.createRole({
             name: "Verified",
