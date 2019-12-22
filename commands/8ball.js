4ffbbd4b264810206bd308e4e5306990c13a1305
G2001H <57664563+G2001H@users.noreply.github.com>
2019-12-22 22:34:37 +0100

    Delete 8ball.js

--- commands/8ball.js
+++
@@ -1,7 +0,0 @@
-const textrandom = ["🎱 **Yes!**","🎱 **Yup!**","🎱 **No!**","🎱 **I Don't Know!**","🎱 **Maybe!**","🎱 **Not Sure!**","🎱 **Nope!**"];
-
-module.exports.run = async (bot, message, args) => {
-    const argumantstwo = args.join(" ");
-    if(!argumantstwo) return message.reply('`;8Ball <Question>`')
-    message.reply(textrandom[Math.floor(Math.random() * textrandom.length)]);
-}
