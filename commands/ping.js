62f70f908dc62336ddd2c7375fab6d5254964909
host324 <59074287+host324@users.noreply.github.com>
2019-12-27 12:36:31 +0100

    Delete ping.js

--- commands/ping.js
+++
@@ -1,9 +0,0 @@
-const Discord = require('discord.js')
-
-module.exports.run = async (bot, message, args) => {
-    let embed = new Discord.RichEmbed()
-    .setColor("RANDOM")
-    .setTitle(`**Pong** 🏓 **${Math.round(bot.ping)} ms**!`)
-    .setFooter('Bot by: G2001H#7660')
-    message.channel.send(embed)
-}
