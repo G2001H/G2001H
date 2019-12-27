8933b43bdf16c7fa0851380f699ba31a038c8962
G2001H <57664563+G2001H@users.noreply.github.com>
2019-12-27 05:14:50 +0100

    Update and rename rolehave.js to memberrole.js

--- commands/memberrole.js
+++ commands/memberrole.js
@@ -3,6 +3,7 @@ const { getMember, formatDate } = require("../functions.js");
 
 module.exports.run = async (bot, message, args) => {
     const MemberName = getMember(message, args.join(" "));
+    if(!MemberName) return message.reply("`;MemberRole <@Member>`")
     const embed = new Discord.RichEmbed()
     .setColor("GREEN")
     .addField("Roles:", MemberName.roles.filter(r => r.id !== message.guild.id).map(r => r).join(", ") || "None")
