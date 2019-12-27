8a0af38a669c65fa41494afb381ee5c07bb2c94e
host324 <59074287+host324@users.noreply.github.com>
2019-12-27 12:34:26 +0100

    Delete serversetname.js

--- commands/serversetname.js
+++
@@ -1,7 +0,0 @@
-module.exports.run = async (bot, message, args) => {
-    if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("**You don't have permissions to use this command!**");
-    const argument = args.join(" ")
-    if(!argument) return message.reply('`;ServerSetName <Text>`')
-    message.guild.setName(argument);
-    message.reply('✅ **Succsessfuly changed server name to:** **__' + argument + '__** ✅')
-}
