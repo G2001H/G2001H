5a09b6df4fd3889bf4c1e2695272bb5b9fdce404
G2001H <57664563+G2001H@users.noreply.github.com>
2019-12-27 05:16:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const sec = Math.floor((ms / 1000) % 60).toString()
 const min = Math.floor((ms / (1000 * 60)) % 60).toString()
 const hrs = Math.floor((ms / (1000 * 60 * 60)) % 60).toString()
 const days = Math.floor((ms / (1000 * 60 * 60 * 60)) % 60).toString()
-return `${days.padStart(1, '0')}:${hrs.padStart(1, '0')}:${min.padStart(1, '0')}:${sec.padStart(1, '0')}`
+return `${days.padStart(2, '0')}:${hrs.padStart(2, '0')}:${min.padStart(2, '0')}:${sec.padStart(2, '0')}`
 }
 
 client.on('ready', () =>{
