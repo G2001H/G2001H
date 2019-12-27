a39bf30cfa7d396973533d3c445800be267af5d1
host324 <59074287+host324@users.noreply.github.com>
2019-12-27 12:34:33 +0100

    Delete say.js

--- commands/say.js
+++
@@ -1,6 +0,0 @@
-module.exports.run = async (bot, message, args) => {
-    const says = args.join(" ");
-    message.delete(0);
-    if(!says) return message.reply("`;Say <Text>`");
-    message.channel.send(says);
-}
