db55f17c1a9b2e84222a6a709303cbcd8ae846ea
G2001H <57664563+G2001H@users.noreply.github.com>
2019-12-27 05:11:13 +0100

    Add files via upload

---
+++ rolehave.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js")
+const { getMember, formatDate } = require("../functions.js");
+
+module.exports.run = async (bot, message, args) => {
+    const MemberName = getMember(message, args.join(" "));
+    const embed = new Discord.RichEmbed()
+    .setColor("GREEN")
+    .addField("Roles:", MemberName.roles.filter(r => r.id !== message.guild.id).map(r => r).join(", ") || "None")
+    .setFooter('Bot by: G2001H#2001')
+    message.channel.send(embed)
+}
