86371b34ca2fd8cf3f62ba326c19b474964fb47f
host324 <59074287+host324@users.noreply.github.com>
2020-01-11 00:53:09 +0100

    Update index.js

--- G2001H-master/index.js
+++ G2001H-master/index.js
@@ -2,6 +2,14 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const pref = require("./package.json")
 const { getMember, formatDate } = require("./functions.js");
+const swearWords = [
+"slut","penis","ass","bitch","bitches","asshole","fuck","fak","fucker","fucking","fuckyou","motherfucker",
+"fakyou","faggot","fag00t","vagina","dick","dicks","pussy","anus","sex","sexy","sexi",
+"seksi","porn","pornography","porno","porns","nudo","nudos","lipshit","boffing","cock",
+"c0ck","dildo","brazzers",,"xnxx","bastard","dickhead","snatch","twat","knob","minge",
+"prick","gash","flaps","punani","clunge","beaver","bloodclaat","bellend","fanny","clunge",];
+const discordlink = ["https://discord.gg/"];
+
 function duration(ms){
 const sec = Math.floor((ms / 1000) % 60).toString()
 const min = Math.floor((ms / (1000 * 60)) % 60).toString()
@@ -88,6 +96,40 @@ client.on("guildMemberRemove", member => {
 })
 
 client.on("message", message => {
+if(swearWords.some(word => message.content.toLowerCase().includes(word))) {
+if(message.guild.ownerID === message.author.id){
+	return;
+}else{
+	const embeds = new Discord.RichEmbed()
+	.setTitle("__Bad word__")
+	.setColor("RANDOM")
+	.addField("**__Member:__**", message.author.tag)
+	.addField("**__Bad Word:__**", message.content)
+	.setFooter("Bot by: G2001H#2001")
+	message.channel.send(embeds).catch(error => console.log(error))
+	message.reply("*Don't say bad words!*").then(sentMessage =>{
+	sentMessage.delete(10000).catch(error => console.log(error))
+	})
+	message.delete(0).catch(error => console.log(error))
+	}
+}
+if(discordlink.some(link => message.content.toLowerCase().includes(link))) {
+if(message.guild.ownerID === message.author.id){
+	return;
+}else{
+	const embeds2 = new Discord.RichEmbed()
+	.setTitle("__Discord Server Link__")
+	.setColor("RANDOM")
+	.addField("**__Member:__**", message.author.tag)
+	.addField("**__Link:__**", message.content)
+	.setFooter("Bot by: G2001H#2001")
+	message.channel.send(embeds2).catch(error => console.log(error))
+	message.reply("*It's not allowed to advertise discord server link!*").then(sentMessage =>{
+	sentMessage.delete(10000).catch(error => console.log(error))
+	})
+	message.delete(0).catch(error => console.log(error))
+	}
+}
 	if (message.author.bot) return;
 	if (message.author.bot || !message.guild) return;
 	if (!message.content.startsWith(pref.prefix)) return;
