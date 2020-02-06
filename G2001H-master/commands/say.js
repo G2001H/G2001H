2af625137e5012a819fd46d17a46d661585890d9
G2001H <57664563+G2001H@users.noreply.github.com>
2020-02-06 22:30:37 +0100

    Update say.js

--- G2001H-master/commands/say.js
+++ G2001H-master/commands/say.js
@@ -1,4 +1,5 @@
 module.exports.run = async (bot, message, args) => {
+    if(!message.member.hasPermission('ADMINISTRATOR')) return message.reply('**You dont have permission to use this command!**');
     const says = args.join(" ");
     message.delete(0);
     if(!says) return message.reply("`;Say <Text>`");
